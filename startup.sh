b719f45f94f1e9ba746363f55d32622741744e96
Riaz Arbi <riazarbi@users.noreply.github.com>
2021-06-10 21:29:17 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+# Get pulling the big guy
 docker pull riazarbi/datasci-r-heavy:focal
+docker run -d --restart=always --name crypto-refresh -v /data/syncthing/crypto:/home/jovyan/crypto riazarbi/datasci-r-heavy:focal /bin/bash -c "bash crypto/refresh.sh" 
