85073d56a53d069d9af5fcf692b0d0101137086c
Riaz Arbi <riazarbi@users.noreply.github.com>
2021-06-11 11:54:33 +0200

    Add crypto-compact

--- startup.sh
+++ startup.sh
@@ -2,3 +2,4 @@
 # Get pulling the big guy
 docker pull riazarbi/datasci-r-heavy:focal
 docker run -d --restart=always --name crypto-refresh -v /data/syncthing/crypto:/home/jovyan/crypto riazarbi/datasci-r-heavy:focal /bin/bash -c "bash crypto/refresh.sh" 
+docker run -d --restart=always --name crypto-compact -v /data/syncthing/crypto:/home/jovyan/crypto riazarbi/datasci-r-heavy:focal /bin/bash -c "bash crypto/compact.sh"
