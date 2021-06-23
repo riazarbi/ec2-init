85f6c2a049e430f60f3cbfb3a62db0bb018c895e
Riaz Arbi <riazarbi@users.noreply.github.com>
2021-06-23 21:13:21 +0200

    Run python in unbuffered mode

--- startup.sh
+++ startup.sh
@@ -10,8 +10,17 @@ docker run -d --restart=always --name crypto-compact -v /data/syncthing/crypto:/
 # Riaz Arbi crypto workloads
 crypto_user=Riaz.Arbi
 # kraken trader
-docker run -d --restart=always --name crypto-kraken-trade-$crypto_user -v /data/projects/crypto:/home/jovyan/crypto -v /data/projects/secrets.json:/home/jovyan/secrets.json $base_image /bin/bash -c "python3 crypto/kraken-buy.py $crypto_user"
+docker run -d --restart=always --name crypto-kraken-trade-$crypto_user \
+-v /data/projects/crypto:/home/jovyan/crypto \
+-v /data/projects/secrets.json:/home/jovyan/secrets.json \
+$base_image /bin/bash -c "python3 -u crypto/kraken-buy.py $crypto_user"
 # kraken transfer
-docker run -d --restart=always --name crypto-kraken-tfer-$crypto_user -v /data/projects/crypto:/home/jovyan/crypto -v /data/projects/secrets.json:/home/jovyan/secrets.json $base_image /bin/bash -c "python3 crypto/kraken-tfer.py $crypto_user"
+docker run -d --restart=always --name crypto-kraken-tfer-$crypto_user \
+-v /data/projects/crypto:/home/jovyan/crypto \
+-v /data/projects/secrets.json:/home/jovyan/secrets.json \
+$base_image /bin/bash -c "python3 -u crypto/kraken-tfer.py $crypto_user"
 # luno trader
-docker run -d --restart=always --name crypto-luno-sell-$crypto_user -v /data/projects/crypto:/home/jovyan/crypto -v /data/projects/secrets.json:/home/jovyan/secrets.json $base_image /bin/bash -c "python3 crypto/luno-sell.py $crypto_user"
+docker run -d --restart=always --name crypto-luno-sell-$crypto_user \
+-v /data/projects/crypto:/home/jovyan/crypto \
+-v /data/projects/secrets.json:/home/jovyan/secrets.json \
+$base_image /bin/bash -c "python3 -u crypto/luno-sell.py $crypto_user"
