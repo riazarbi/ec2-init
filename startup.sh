d2d47f4a67a5b66c2ced22341618bd671b174a11
Riaz Arbi <riazarbi@users.noreply.github.com>
2021-11-04 08:48:50 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Get pulling the big guy
-base_image=riazarbi/datasci-r-heavy:20210519
-docker pull $base_image
+#base_image=riazarbi/datasci-r-heavy:20210519
+#docker pull $base_image
 
 # Sinking market data for backtesting
 #docker run -d --restart=always --name crypto-refresh -v /data/syncthing/crypto:/home/jovyan/crypto $base_image /bin/bash -c "bash crypto/refresh.sh" 
@@ -9,24 +9,24 @@ docker pull $base_image
 #docker run -d --restart=always --name crypto-compact -v /data/syncthing/crypto:/home/jovyan/crypto $base_image /bin/bash -c "bash crypto/compact.sh"
 
 # Riaz Arbi crypto workloads
-crypto_user=Riaz.Arbi
+#crypto_user=Riaz.Arbi
 # kraken trader
-docker run -d --restart=always --name crypto-kraken-trade-$crypto_user \
--v /data/projects/crypto:/home/jovyan/crypto:ro \
--v /data/projects/secrets.json:/home/jovyan/secrets.json:ro \
-$base_image /bin/bash -c "python3 -u crypto/kraken-buy.py $crypto_user"
+#docker run -d --restart=always --name crypto-kraken-trade-$crypto_user \
+#-v /data/projects/crypto:/home/jovyan/crypto:ro \
+#-v /data/projects/secrets.json:/home/jovyan/secrets.json:ro \
+#$base_image /bin/bash -c "python3 -u crypto/kraken-buy.py $crypto_user"
 # kraken gbp trader
-docker run -d --restart=always --name crypto-kraken-gbp-trade-$crypto_user \
--v /data/projects/crypto:/home/jovyan/crypto:ro \
--v /data/projects/secrets.json:/home/jovyan/secrets.json:ro \
-$base_image /bin/bash -c "python3 -u crypto/kraken-gbp-buy.py $crypto_user"
+#docker run -d --restart=always --name crypto-kraken-gbp-trade-$crypto_user \
+#-v /data/projects/crypto:/home/jovyan/crypto:ro \
+#-v /data/projects/secrets.json:/home/jovyan/secrets.json:ro \
+#$base_image /bin/bash -c "python3 -u crypto/kraken-gbp-buy.py $crypto_user"
 # kraken transfer
-docker run -d --restart=always --name crypto-kraken-tfer-$crypto_user \
--v /data/projects/crypto:/home/jovyan/crypto:ro \
--v /data/projects/secrets.json:/home/jovyan/secrets.json:ro \
-$base_image /bin/bash -c "python3 -u crypto/kraken-tfer.py $crypto_user"
+#docker run -d --restart=always --name crypto-kraken-tfer-$crypto_user \
+#-v /data/projects/crypto:/home/jovyan/crypto:ro \
+#-v /data/projects/secrets.json:/home/jovyan/secrets.json:ro \
+#$base_image /bin/bash -c "python3 -u crypto/kraken-tfer.py $crypto_user"
 # luno trader
-docker run -d --restart=always --name crypto-luno-sell-$crypto_user \
--v /data/projects/crypto:/home/jovyan/crypto:ro \
--v /data/projects/secrets.json:/home/jovyan/secrets.json:ro \
-$base_image /bin/bash -c "python3 -u crypto/luno-sell.py $crypto_user"
+#docker run -d --restart=always --name crypto-luno-sell-$crypto_user \
+#-v /data/projects/crypto:/home/jovyan/crypto:ro \
+#-v /data/projects/secrets.json:/home/jovyan/secrets.json:ro \
+#$base_image /bin/bash -c "python3 -u crypto/luno-sell.py $crypto_user"
